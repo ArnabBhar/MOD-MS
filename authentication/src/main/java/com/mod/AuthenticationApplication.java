e954a157b79dac9ccf114e0c7ea0724955be870c
Karun333 <karunjossy333@gmail.com>
2019-10-05 09:48:38 +0530

    Working authentication micro service

--- authentication/src/main/java/com/mod/AuthenticationApplication.java
+++ authentication/src/main/java/com/mod/AuthenticationApplication.java
@@ -2,9 +2,10 @@ package com.mod;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class AuthenticationApplication {
 
 	public static void main(String[] args) {
